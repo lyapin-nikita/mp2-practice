0f22a599f086dddb783b6481542e347d48d415a2
Lyapin Nikita <paladins392@gmail.com>
2023-10-18 09:53:02 +0300

    5 mistakes
    check tests

--- LyapinNA/01_lab/src/tbitfield.cpp
+++ LyapinNA/01_lab/src/tbitfield.cpp
@@ -9,88 +9,141 @@
 
 TBitField::TBitField(int len)
 {
+    if (len < BitLen) throw "the len of massive couldn't be less then 0";
+    BitLen = len;
+    MemLen = (BitLen - 1) / (sizeof(TELEM) * 8) + 1;
+    pMem = new TELEM[MemLen];
+    for (int i = 0; i < MemLen; ++i) pMem[i]=0;
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+    BitLen = bf.BitLen;
+    MemLen = bf.MemLen;
+    pMem = new TELEM[MemLen];
+    for (int i = 0; i < MemLen; ++i) pMem[i] = bf.pMem[i];
 }
 
-TBitField::~TBitField()
-{
-}
+TBitField::~TBitField() { delete[] this->pMem; }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
-    throw "Method is not implemented";
+    if(n<0 || n>BitLen-1) throw "Method is not implemented";
+    return n/sizeof(TELEM)*8;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
-    throw "Method is not implemented";
+    if (n<0 || n>BitLen - 1) throw "Method is not implemented";
+    size_t justnumforfirstlab = 1;
+    return justnumforfirstlab << sizeof(TELEM) * 8 - n% (sizeof(TELEM) * 8);
 }
 
 // доступ к битам битового поля
 
 int TBitField::GetLength(void) const // получить длину (к-во битов)
-{
-  return 0;
+{ 
+    return BitLen;
 }
 
 void TBitField::SetBit(const int n) // установить бит
 {
-    throw "Method is not implemented";
+    if (n < 0 || n > BitLen - 1) throw "Method is not implemented";
+    pMem[GetMemIndex(n)] |= GetMemMask(n);
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
 {
-    throw "Method is not implemented";
+    if (n < 0 || n > BitLen - 1) throw "Method is not implemented";
+    pMem[GetMemIndex(n)] &= ~GetMemMask(n);
 }
 
 int TBitField::GetBit(const int n) const // получить значение бита
 {
-  return 0;
+    if (n < 0 || n > BitLen - 1) throw "Method is not implemented";
+
+    if ((pMem[GetMemIndex(n)] & GetMemMask(n)) == GetMemMask(n)) return 1;
+    else return 0;
 }
 
 // битовые операции
 
 TBitField& TBitField::operator=(const TBitField &bf) // присваивание
 {
-    throw "Method is not implemented";
+    //throw "Method is not implemented";
+    BitLen = bf.BitLen;
+    MemLen = bf.MemLen;
+    for (int i(0); i < MemLen; ++i) pMem[i] = bf.pMem[i];
+    return *this;
 }
 
 int TBitField::operator==(const TBitField &bf) const // сравнение
 {
-  return 0;
+    if ((BitLen != bf.BitLen) || (MemLen != bf.MemLen)) return false;
+    for (int i(0); i < MemLen; ++i) if (pMem[i] != bf.pMem[i]) return false;
+    return true;
 }
 
 int TBitField::operator!=(const TBitField &bf) const // сравнение
 {
-  return 0;
+    return !(*this == bf);
 }
 
 TBitField TBitField::operator|(const TBitField &bf) // операция "или"
 {
-    throw "Method is not implemented";
+    const int newBitLen = min(BitLen, bf.BitLen);
+    TBitField result(newBitLen);
+    for (int i(0); i < newBitLen; ++i)
+    {
+        if (GetBit(i) == bf.GetBit(i) == 0) result.ClrBit(i);
+        else result.SetBit(i);
+    }
+    return result;
 }
 
 TBitField TBitField::operator&(const TBitField &bf) // операция "и"
 {
-    throw "Method is not implemented";
+    //throw "Method is not implemented";
+    const int newBitLen = min(BitLen, bf.BitLen);
+    TBitField result(newBitLen);
+    for (int i(0); i < newBitLen; ++i)
+    {
+        if (GetBit(i) == bf.GetBit(i) == 1) result.SetBit(i);
+        else result.ClrBit(i);
+    }
+    return result;
 }
 
 TBitField TBitField::operator~(void) // отрицание
 {
-    throw "Method is not implemented";
+    //throw "Method is not implemented";
+    TBitField result(BitLen);
+    for (int i(0); i < BitLen; ++i)
+    {
+        if (!GetBit(i)) result.SetBit(i);
+    }
+    return result;
 }
 
 // ввод/вывод
 
 istream &operator>>(istream &istr, TBitField &bf) // ввод
 {
-    throw "Method is not implemented";
+    //throw "Method is not implemented";
+    for (int i = 0; i < bf.GetLength(); ++i)
+    {
+        int tmp;
+        istr >> tmp;
+        if (tmp) bf.SetBit(i);
+        else bf.ClrBit(i);
+    }
+    return istr;
 }
 
 ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
 {
-    throw "Method is not implemented";
+    //throw "Method is not implemented";
+    for (int i = 0; i < bf.BitLen; ++i) ostr << bf.GetBit(i);
+    ostr << endl;
+    return ostr;
 }
--- LyapinNA/01_lab/src/tset.cpp
+++ LyapinNA/01_lab/src/tset.cpp
@@ -7,95 +7,130 @@
 
 #include "tset.h"
 
-TSet::TSet(int mp) : BitField(-1)
+TSet::TSet(int mp) : BitField(mp)
 {
+    MaxPower = mp;
 }
 
 // конструктор копирования
-TSet::TSet(const TSet &s) : BitField(-1)
+TSet::TSet(const TSet &s) : BitField(s.BitField)
 {
+    MaxPower = s.MaxPower;
 }
 
 // конструктор преобразования типа
-TSet::TSet(const TBitField &bf) : BitField(-1)
+TSet::TSet(const TBitField &bf) : BitField(bf), MaxPower(bf.GetLength())
 {
 }
 
 TSet::operator TBitField()
 {
-    throw "Method is not implemented";
+    return BitField;
 }
 
 int TSet::GetMaxPower(void) const // получить макс. к-во эл-тов
 {
-    throw "Method is not implemented";
+    return MaxPower;
 }
 
 int TSet::IsMember(const int Elem) const // элемент множества?
 {
-    return 0;
+    if (Elem < MaxPower || Elem > MaxPower - 1) throw "Method is not implemented";
+    return BitField.GetBit(Elem);
 }
 
 void TSet::InsElem(const int Elem) // включение элемента множества
 {
-    throw "Method is not implemented";
+    if (Elem < MaxPower || Elem > MaxPower - 1) throw "Method is not implemented";
+    BitField.SetBit(Elem);
 }
 
 void TSet::DelElem(const int Elem) // исключение элемента множества
 {
-    throw "Method is not implemented";
+    if (Elem < MaxPower || Elem > MaxPower - 1) throw "Method is not implemented";
+    BitField.ClrBit(Elem);
 }
 
 // теоретико-множественные операции
 
 TSet& TSet::operator=(const TSet &s) // присваивание
 {
-    throw "Method is not implemented";
+    MaxPower = s.MaxPower;
+    BitField = s.BitField;
+    return *this;
 }
 
 int TSet::operator==(const TSet &s) const // сравнение
 {
-    return 0;
+    if (MaxPower != s.MaxPower) return false;
+    if (BitField != s.BitField) return false;
+    return true;
 }
 
 int TSet::operator!=(const TSet &s) const // сравнение
 {
-    throw "Method is not implemented";
+    return !(*this == s);
 }
 
 TSet TSet::operator+(const TSet &s) // объединение
 {
-    throw "Method is not implemented";
+    //throw "Method is not implemented";
+    TSet result(max(MaxPower, s.MaxPower));
+    result.BitField = BitField | s.BitField;
+    return result;
 }
 
 TSet TSet::operator+(const int Elem) // объединение с элементом
 {
-    throw "Method is not implemented";
+    if (Elem < MaxPower || Elem > MaxPower - 1) throw "Method is not implemented";
+    TSet result(*this);
+    result.BitField.SetBit(Elem);
+    return result;
 }
 
 TSet TSet::operator-(const int Elem) // разность с элементом
 {
-    throw "Method is not implemented";
+    if (Elem < MaxPower || Elem > MaxPower - 1) throw "Method is not implemented";
+    TSet result(*this);
+    result.BitField.ClrBit(Elem);
+    return result;
 }
 
 TSet TSet::operator*(const TSet &s) // пересечение
 {
-    throw "Method is not implemented";
+    //throw "Method is not implemented";
+    TSet result(max(MaxPower, s.MaxPower));
+    result.BitField = BitField & s.BitField;
+    return result;
 }
 
 TSet TSet::operator~(void) // дополнение
 {
-    throw "Method is not implemented";
+    //throw "Method is not implemented";
+    TSet result(*this);
+    result.BitField = ~BitField;
+    return result;
 }
 
 // перегрузка ввода/вывода
 
 istream &operator>>(istream &istr, TSet &s) // ввод
 {
-    throw "Method is not implemented";
+    //throw "Method is not implemented";
+    TELEM tmp;
+    istr >> tmp;
+    if (tmp < 0 || tmp >= s.GetMaxPower())
+        throw "ERROR_IN_CIN_NUMBER_IS_OUT_OF_RANGE";
+    s.InsElem(tmp);
+    return istr;
 }
 
 ostream& operator<<(ostream &ostr, const TSet &s) // вывод
 {
-    throw "Method is not implemented";
+    //throw "Method is not implemented";
+    for (int i = 0; i < s.GetMaxPower(); ++i)
+        if (s.IsMember(i))
+            ostr << i << " ";
+    return ostr;
 }
+
